e0fd6c7838eedc8dfae97cd19e2842571cb8bd59
pedro <pedro-gomes17@hotmail.com>
2020-01-19 00:01:34 -0300

    Web socket - mobile

---
+++ mobile/src/services/socket.js
@@ -0,0 +1,31 @@
+import socketio from 'socket.io-client';
+
+const socket = socketio('http://192.168.25.6:3333', {
+    autoConnect:false,
+})
+
+function subscribeToNewDev(subscribeFunction) {
+    socket.on('new-dev', subscribeFunction)
+}
+
+function connect(latitude, longitude, techs){
+    //Mandar pro backend
+    socket.io.opts.query = {
+        latitude,
+        longitude,
+        techs,
+    }
+    socket.connect()
+}
+
+function disconnect(){
+    if(socket.connected) {
+        socket.disconnect()
+    }
+}
+
+export {
+    connect,
+    disconnect,
+    subscribeToNewDev
+}
